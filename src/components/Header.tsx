4b6cd5f0cf28043d63cd6c5a5313b173a712e48d
Cláudio Pedro Mindouro <claudiopedrom@gmail.com>
2023-08-15 17:34:48 +0200

    feat: player split components

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">Lorem Ipsum</h1>
+      <span className="text-sm text-zinc-400">Lorem Ipsum</span>
+    </div>
+  );
+}
