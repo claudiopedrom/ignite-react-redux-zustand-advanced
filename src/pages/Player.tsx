15888d1f19b00bd1b6c634deb4a4248e28e30b94
Cláudio Pedro Mindouro <claudiopedrom@gmail.com>
2023-08-20 15:19:39 +0200

    feat: global lesson hook

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import { MessageCircle } from "lucide-react";
 
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
 import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
 
 export function Player() {
   // It is important to not return the entire slice, but only the part of the state that is needed.
@@ -11,6 +13,12 @@ export function Player() {
   const modules = useAppSelector((state) => {
     return state.player.course.modules;
   });
+
+  const { currentLesson } = useCurrentLesson();
+
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`;
+  }, [currentLesson]);
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
